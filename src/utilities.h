5d46b175762256a6d13c7d95dac7e1f0452dcf81
Ignacio <50671274+IgnacioOchoa@users.noreply.github.com>
2023-07-30 20:46:22 +0200

    Create utilities.h

---
+++ src/utilities.h
@@ -0,0 +1,55 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <utility>
+#include <regex>
+#include <iomanip>
+#include <cmath>
+
+constexpr char TOK = '#';
+constexpr short leftFlag =      0b00001;
+constexpr short rightFlag =     0b00010;
+constexpr short topFlag =       0b00100;
+constexpr short botFlag =       0b01000;
+constexpr short internalFlag =  0b10000;
+
+bool checkFile(std::string fileName);
+
+std::pair<short,short> getSectionLimitLines(std::string fileName);
+
+void computeQualifiedBlocks(std::string fileName, 
+                            std::vector<std::pair<short,std::vector<short>>>& blocks, 
+                            std::pair<short, short> sectionLimitLines);
+                            
+void computeRefinedBlocks(const std::vector<std::pair<short,std::vector<short>>>& qualifiedBlocks, 
+                                std::vector<std::array<double,4>>& refinedBlocks,
+                                double blockWidth,
+                                double blockHeight);
+
+std::vector<short> categorizeLine(  const std::string& curLine, 
+                                    const std::string& prevLine, 
+                                    const std::string& nexLine,
+                                    bool isFirstLine,
+                                    bool isLastLine);
+                                    
+short calculateMaxWidthBlocks(const std::vector<std::pair<short, 
+                               std::vector<short>>>& blocks);
+                               
+double calculateVerticalCenter(const std::array<double,4>& sides);
+
+double calculatArea(const std::array<double,4>& sides);
+                                    
+double calculateCentroid(const std::vector<std::array<double,4>>& refinedBlocks);
+                              
+double calculateInertia(const std::vector<std::array<double,4>>& refinedBlocks, double centroidYPosition);
+
+void showResults(double centroidY, double inertiaY, double maxStress);
+
+//Function for debugging
+void printMapping(const std::vector<std::pair<short, std::vector<short>>>& blocks);
+                    
+#endif
